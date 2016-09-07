ee6306228a5cabd221b9fe8feb03100f6a73bcf4
pmore362 <pmore362@outlook.com>
2016-09-07 11:22:52 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,7 +1,4 @@
-/*(Separating Digits) Write program segments that accomplish each of the following:
-a) Calculate the integer part of the quotient when integer a is divided by integer b.
-b) Calculate the integer remainder when integer a is divided by integer b.
-c) Use the program pieces developed in a) and b) to write a function that inputs an integer
+/*(Separating Digits)write a function that inputs an integer
 between 1 and 32767 and prints it as a series of digits,with two spaces between each digit.
 For example, the integer 4562 should be printed as: 4 5 6 2*/
 #include<stdio.h>
